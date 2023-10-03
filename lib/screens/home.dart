a987d12522f30021d031b89127aaa6f490397d5d
tiamo.405 <nam05052002@gmail.com>
2023-10-03 22:28:26 +0700

    delete and add item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,6 +12,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
+  List<ToDo> _foundToDo = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundToDo = todosList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +44,11 @@ class _HomeState extends State<Home> {
                             fontSize: 30, fontWeight: FontWeight.w500),
                       ),
                     ),
-                    for (ToDo todoo in todosList) 
+                    for (ToDo todoo in _foundToDo.reversed) 
                     ToDoItem(
                       todo: todoo,
                       onToDoChanged: _handleToDoChange,
-                      onDeleteItem: (){},
+                      onDeleteItem: _deleteToDoItem,
                       )
                   ],
                 ))
@@ -70,6 +78,7 @@ class _HomeState extends State<Home> {
                           borderRadius:  BorderRadius.circular(10)
                     ),
                     child: TextField(  
+                      controller: _todoController,
                       decoration:  InputDecoration(  
                         hintText:  'Add a new todo item',
                         border:  InputBorder.none,
@@ -87,7 +96,9 @@ class _HomeState extends State<Home> {
                       '+',
                       style:  TextStyle(fontSize: 40),
                     ),
-                    onPressed: () {  },
+                    onPressed: () { 
+                      _addToDoItem(_todoController.text);
+                    },
                     style: ElevatedButton.styleFrom(  
                       backgroundColor: tdBlue,
                       minimumSize: Size(50, 50),
@@ -106,15 +117,40 @@ void _handleToDoChange( ToDo todo){
   setState(() {
     todo.isDone = !todo.isDone;
   });
-  
 }
 
+void _deleteToDoItem(String id){  
+  setState(() {
+    todosList.removeWhere((item) => item.id == id);
+  });
+}
+
+void _addToDoItem(String toDo){
+  setState(() {
+    todosList.add(ToDo(id: DateTime.now().millisecondsSinceEpoch.toString(), 
+    todoText: toDo));
+  });
+  _todoController.clear();
+}
+
+void _runFilter(String enterKeyword){
+  List<ToDo> results = [];
+  if (enterKeyword.isEmpty){
+    results = todosList;
+  } else{
+     results = todosList.where((item) => item.todoText!.toLowerCase().contains(enterKeyword.toLowerCase())).toList();
+  }
+  setState(() {
+    _foundToDo = results;
+  });
+}
   Widget searchBox() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: TextField(
+        onChanged: (value) => _runFilter(value),
         decoration: InputDecoration(
           contentPadding: EdgeInsets.all(0),
           prefixIcon: Icon(
